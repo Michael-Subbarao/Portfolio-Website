1163200599f513a8ea4eb9a0d01c26a1e2f20b42
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-05-18 23:27:47 -0400

    switched from bootstrapp sass--> tailwind postcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.scss'
+import '../styles/globals.css'
 import {ThemeProvider} from 'next-themes'
-
+import { createGlobalStyle } from 'styled-components'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider enableSystem = {true} attribute = "class">
       <Component {...pageProps} />
     </ThemeProvider>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Home.module.css'
 import {useTheme} from 'next-themes'
+
 
 export default function Home() {
   const {theme, setTheme} = useTheme();
