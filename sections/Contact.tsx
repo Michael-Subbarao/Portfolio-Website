a56d7df884c0232cc08a5cc0ff3412eed8fc8611
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-06-03 15:03:11 -0400

    Contact form now sends an email to me via EmailJs

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,19 +1,36 @@
-import { useRouter } from 'next/router'
-import Button from "../components/Button"
+import emailJs from '@emailjs/browser';
+import { useState } from 'react';
 
-const Contact = () => (
-  
+const Contact = () => {
+  const [result, setResult] = useState('');
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      emailJs.sendForm('service_gz2rdem', 'template_ztc1efk', e.target, 'FRSf_g35L7tSkmjmH')
+      .then(res => {
+        setResult('Message sent successfully')
+      }
+      )
+      .catch(err => {
+        setResult('Something went wrong sending your message, please try again')
+      }
+      )
+      e.target.reset();
+      setTimeout(()=>{},3000);
+      setResult('');
+    }
+  return (  
   <div className="p-6 md:mx-48 text-slate-700 font-semibold dark:text-slate-400">
+    {result && <p className="text-sm text-gray-600 dark:text-slate-50 mt-4 mb-4 md:mb-8">{result}</p>}
     <h1 className="ml-1  text-7xl font-bold transition ease-in-out">Contact <span className="text-blue-600 dark:text-rose-600 transition ease-in-out">Me</span></h1>
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="grid grid-cols-2 gap-4">
         <div className="flex flex-col">
-          <label htmlFor="first-name">First name<span className="text-red-400">*</span></label>
-          <input type="text" id="first-name" name="first-name" className="form-input px-3 py-2 rounded-md" required />
+          <label htmlFor="firstName">First name<span className="text-red-400">*</span></label>
+          <input type="text" id="first-name" name="firstName" className="form-input px-3 py-2 rounded-md" required />
         </div>
         <div className="flex flex-col">
-          <label htmlFor="last-name">Last name<span className="text-red-400">*</span></label>
-          <input type="text" id="last-name" name="last-name" className="form-input px-3 py-2 rounded-md" required />
+          <label htmlFor="lastName">Last name<span className="text-red-400">*</span></label>
+          <input type="text" id="last-name" name="lastName" className="form-input px-3 py-2 rounded-md" required />
         </div>
         <div className="flex flex-col">
           <label htmlFor="email">Email<span className="text-red-400">*</span></label>
@@ -43,12 +60,13 @@ const Contact = () => (
         </div>
       </div>
       <div className="flex justify-end py-4">
-        <button type="submit" className="bg-rose-600 text-slate-800 dark:bg-rose-600 text-slate-200 px-6 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:ring-2 hover:ring-gray-300 text-white font-bold py-2 px-4 rounded focus:ring focus:ring-blue-300 hover:bg-blue-500">
+        <button type="submit"  className="bg-rose-600 text-slate-800 dark:bg-rose-600 text-slate-200 px-6 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:ring-2 hover:ring-gray-300 text-white font-bold py-2 px-4 rounded focus:ring focus:ring-blue-300 hover:bg-blue-500">
           Submit
         </button>
       </div>
     </form>
   </div>
 );
+  }
 
 export default Contact;
